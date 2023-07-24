c4e34a6f62c39b93541ad797fc7adef5384a1573
strawhatrag <sreeragbharathan791@gmail.com>
2023-07-24 13:03:36 +0530

    updated to mongo

--- app.js
+++ app.js
@@ -1,76 +1,92 @@
-//jshint esversion:6
+// jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
+// Sample content for different pages
 const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare...";
 const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque...";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien...";
 
 const app = express();
 
-const posts = [];
+// Connect to the MongoDB database named 'blog' on the local machine
+mongoose.connect("mongodb://127.0.0.1:27017/blog");
+const blogSchema = {
+  title: String,
+  body: String,
+};
+const Blog = mongoose.model("Blog", blogSchema);
 
-// make sure views folder is visible
+// Set the view engine to use EJS templates
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  // pass in {key: value}
-  res.render("home", { startingContent: homeStartingContent, posts: posts });
+// Home Page
+app.get("/", async (req, res) => {
+  // Fetch all blog posts from the database
+  const blogPosts = await Blog.find({});
+
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: blogPosts,
+  });
 });
 
-//  About Page
+// About Page
 app.get("/about", (req, res) => {
-  // pass in {key: value}
   res.render("about", { myaboutContent: aboutContent });
 });
 
 // Contact Page
 app.get("/contact", (req, res) => {
-  // pass in {key: value}
   res.render("contact", { mycontactContent: contactContent });
 });
 
 // Compose Page
 app.get("/compose", (req, res) => {
-  // pass in {key: value}
   res.render("compose");
 });
 
-// compose post
-app.post("/compose", (req, res) => {
-  // notice postTitle is the input name we gave
+// Compose Post - Handle form submission
+app.post("/compose", async (req, res) => {
   const { postTitle, postContent } = req.body;
-  const newPost = { postTitle, postContent };
 
-  posts.push(newPost);
-  // redirects to other pages
+  // Create a new blog post and save it to the database
+  const post = new Blog({
+    title: postTitle,
+    body: postContent,
+  });
+  await post.save();
+
+  // Redirect back to the home page after successful post creation
   res.redirect("/");
 });
 
-// params
-app.get("/:id", (req, res) => {
-  //      Lodash   -- cleans up the params
-  // _.lowerCase('--Foo-Bar--');    => 'foo bar'  , day1 becomes day 1
-  let title = _.lowerCase(req.params.id);
-  let page = posts.find((post) => post.postTitle === title);
+// View a specific blog post using the post title as a parameter
+app.get("/:id", async (req, res) => {
+  // Get the title from the URL and convert it to lowercase using Lodash
+  let pageTitle = _.lowerCase(req.params.id);
+
+  // Find the blog post with the matching title in the database
+  let page = await Blog.findOne({ title: pageTitle });
 
   if (page) {
     res.render("post", {
-      postTitle: page.postTitle,
-      postContent: page.postContent,
+      postTitle: page.title,
+      postContent: page.body,
     });
   }
 });
 
+// Start the server on port 3000
 app.listen(3000, function () {
   console.log("listening on http://localhost:3000");
 });
